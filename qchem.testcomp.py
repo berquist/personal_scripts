d39f38fdec6a95d3aee5d3f951eb232ba268718e
Eric Berquist <erb74@pitt.edu>
2014-03-16 14:14:42 -0400

    Output wasn't being appended to a file through stdout properly.

--- qchem.testcomp.py
+++ qchem.testcomp.py
@@ -7,7 +7,7 @@ import os
 import socket
 import shutil
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description='Compare Q-Chem test outputs with reference outputs. $QC, $QCREF, and $QCPLATFORM must be defined.')
 parser.add_argument('--copy', action='store_true', help='Copy the problem outputs to ~/qchem_test_outputs with the appropriate hostname.')
 args = parser.parse_args()
 copy = args.copy
@@ -28,7 +28,17 @@ for line in tablecompare.splitlines():
         continue
     elif l[0] == '[' and l[1][-4:] == '.out':
         command2 = command2_stub + l[1]
-        subprocess.call(command2.split())
+        # The monCompare script will return 1 if outputs differ; who cares,
+        # we just want the output.
+        try:
+            diff = subprocess.check_output(command2.split())
+        except subprocess.CalledProcessError as e:
+            diff = e.output
+        print '=========='
+        print 'job:', l[1]
+        print diff
+        print '=========='
+        print '\n'
         if copy:
             if not os.path.exists(output_path):
                 os.makedirs(output_path)
