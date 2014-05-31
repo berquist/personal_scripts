9a8df0bedb47097dc8ba40ab7098e52dcd67d6c6
Eric Berquist <erb74@pitt.edu>
2014-05-31 14:29:36 -0400

    Determine if any input and output files are orphans.

---
+++ match_input_output.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+"""match_input_output.py: Given input and output file extensions, determine
+which ones don't have matching partners (in the current directory),
+and print the result.
+"""
+
+import argparse
+import os
+import glob
+
+parser = argparse.ArgumentParser()
+parser.add_argument('ext_inp')
+parser.add_argument('ext_out')
+args = parser.parse_args()
+ext_inp = args.ext_inp
+ext_out = args.ext_out
+
+# Get the results from `ls`:
+directory = os.getcwd()
+inputs = [os.path.basename(f) for f in glob.glob(directory + '/*.' + ext_inp)]
+outputs = [os.path.basename(f) for f in glob.glob(directory + '/*.' + ext_out)]
+
+# How should we compare the elements in each?
+# Turn everything into stubs (no extensions):
+inputs_stubs = [os.path.splitext(f)[0] for f in inputs]
+outputs_stubs = [os.path.splitext(f)[0] for f in outputs]
+
+# Loop through each list of stubs separately.
+for iidx, istub in enumerate(inputs_stubs):
+    if istub not in outputs_stubs:
+        print inputs[iidx]
+for oidx, ostub in enumerate(outputs_stubs):
+    if ostub not in inputs_stubs:
+        print outputs[oidx]
