c373e06dc012fb21efec6d20722b4b496b917275
Eric Berquist <erb74@pitt.edu>
2014-09-11 13:15:29 -0400

    handle 'quality' of POV-Ray rendering

--- povray.render_vmd_inputs.py
+++ povray.render_vmd_inputs.py
@@ -3,8 +3,25 @@
 '''povray.render_vmd_inputs.py: Write a bash script to render all VMD-generated
 POV-Ray files in a directory.'''
 
+import argparse
 from glob import glob
 import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--quality', choices=['vmd', 'lq', 'hq'])
+args = parser.parse_args()
+quality = args.quality
+
+runstring = dict()
+# need to silence the display (+D -> -D) so we can run over SSH
+runstring['vmd'] = 'povray +W{width} +H{height} -I{filename} -O{filename}.tga -D +X +A +FT'
+runstring['lq'] = ''
+runstring['hq'] = 'povray +W{width} +H{height} -I{filename} -O{filename}.tga -D +X +C +A +AM2 +R9 +FT +UA +Q11'
+
+if not quality:
+    quality = 'vmd'
+
+convertstring = 'convert {filename}.tga {filename}.png'
 
 bashfilename = 'povray.bash'
 with open(bashfilename, 'wb') as bashfile:
@@ -17,6 +34,6 @@ with open(bashfilename, 'wb') as bashfile:
                     width = line.split()[3][2:]
                     height = line.split()[4][2:]
                     break
-        runstring = 'povray +W{width} +H{height} -I{filename} -O{filename}.png -D +X +C +A +AM2 +R9 +FN10 +UA +Q11'
-        bashfile.write(''.join([runstring.format(width=width, height=height, filename=povrayinputfilename), '\n']))
+        bashfile.write(''.join([runstring[quality].format(width=width, height=height, filename=povrayinputfilename), '\n']))
+        bashfile.write(''.join([convertstring.format(filename=povrayinputfilename), '\n']))
 os.chmod(bashfilename, 0755)
