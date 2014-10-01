063ae0d8f6640faa301070e56e329f2a3acd7b4d
Eric Berquist <erb74@pitt.edu>
2014-10-01 13:20:56 -0400

    run all the Q-Chem NMR test jobs

---
+++ qchem.run_nmr_test_jobs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+jobnames=(hcf3.den.dft.nmr hcf3.den.nmr hcf3.mo.nmr hcf3.nmr-dft nmr2 NMR nmrref)
+
+cd $QC/test
+
+for job in ${jobnames[*]}
+do
+    rm ${job}.out
+    qchem ${job}.in ${job}.out
+done
