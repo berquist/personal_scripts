d99806b7517a5b9891cd226adef1b0b3ad135958
Eric Berquist <erb74@pitt.edu>
2014-10-31 14:07:28 -0400

    start handling MR jobs

--- orca_print_gtot.py
+++ orca_print_gtot.py
@@ -13,13 +13,25 @@ if __name__ == '__main__':
 
     for outputfilename in outputfilenames:
         with open(outputfilename) as outputfile:
+            match = False
             for line in outputfile:
+                # single-reference calculations
                 if 'g(tot)' in line:
                     print(outputfilename)
                     sline = line.split()
+                    match = True
                     g_1, g_2, g_3, g_iso = float(sline[1]), float(sline[2]), float(sline[3]), float(sline[5])
-                    g_perp = (g_1 + g_2) / 2
-                    g_para = g_3
-                    print(' {:<11.8f} {:<11.8f} {:<11.8f}'.format(g_1, g_2, g_3))
-                    print(' g_para: {:<11.8f} g_perp: {:<11.8f} g_iso: {:<11.8f}'.format(g_para, g_perp, g_iso))
                     break
+                # multi-reference calculations
+                if 'g-factors:' in line:
+                    print(outputfilename)
+                    line = next(outputfile)
+                    sline = line.split()
+                    match = True
+                    g_1, g_2, g_3, g_iso = float(sline[0]), float(sline[1]), float(sline[2]), float(sline[-1])
+                    break
+            if match:
+                g_perp = (g_1 + g_2) / 2
+                g_para = g_3
+                print(' {:<11.8f} {:<11.8f} {:<11.8f}'.format(g_1, g_2, g_3))
+                print(' g_para: {:<11.8f} g_perp: {:<11.8f} g_iso: {:<11.8f}'.format(g_para, g_perp, g_iso))
