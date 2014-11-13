0352a56162cf4c4cfbdb68cf1b06faaf8851b899
Eric Berquist <erb74@pitt.edu>
2014-11-12 22:59:17 -0500

    clean up after ourselves

--- update_python.sh
+++ update_python.sh
@@ -5,9 +5,15 @@ module load python/anaconda
 conda update --yes conda
 conda update --yes anaconda
 conda update --yes accelerate
+conda clean --yes --tarballs
+conda clean --yes --packages
+conda clean --yes --lock
 
 module purge
 module load python/anaconda3
 conda update --yes conda
 conda update --yes anaconda
 conda update --yes accelerate
+conda clean --yes --tarballs
+conda clean --yes --packages
+conda clean --yes --lock
