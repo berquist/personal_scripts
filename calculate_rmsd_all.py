c1ec9c120f5f85d7570a8f597c603b596f987ca3
Eric Berquist <erb74@pitt.edu>
2015-01-18 13:38:57 -0500

    calculate the RMSD between every pair of XYZ files in a directory

---
+++ calculate_rmsd_all.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+'''calculate_rmsd_all.py: Call the `calculate_rmsd` Python script on
+every possible pair of XYZ files in the current working directory.
+'''
+
+from __future__ import print_function
+from glob import glob
+import subprocess as sp
+
+xyzfilenames = glob('*.xyz')
+print(xyzfilenames)
+
+for i in range(len(xyzfilenames)):
+    for j in range(i):
+        if i != j:
+            fname1 = xyzfilenames[i]
+            fname2 = xyzfilenames[j]
+            print('file 1: {}'.format(fname1))
+            print('file 2: {}'.format(fname2))
+            sp.call(['calculate_rmsd', fname1, fname2])
+            print('=' * 78)
