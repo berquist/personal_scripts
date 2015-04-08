a14d9147ee5cb6b8e3c6aa8de2e52c61b95d9775
Eric Berquist <erb74@pitt.edu>
2015-04-08 13:55:11 -0400

    Unnecessary variable

--- frank/qfilter.py
+++ frank/qfilter.py
@@ -12,9 +12,8 @@ import subprocess as sp
 parser = argparse.ArgumentParser()
 parser.add_argument('user', type=str)
 args = parser.parse_args()
-user = args.user
 
-stream = sp.check_output(["qstat", "-a"]).decode("utf-8")
-job_lines = [line for line in stream.split('\n') if line.find(user) == -1]
+stream = sp.check_output(['qstat', '-a']).decode('utf-8')
+job_lines = [line for line in stream.split('\n') if line.find(args.user) == -1]
 for line in job_lines:
     print(line)
