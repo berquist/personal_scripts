9602142821e64f757710e1acaa4df875e8bb3aaa
Eric Berquist <erb74@pitt.edu>
2015-07-06 21:12:13 -0400

    Handle normalization properly, add a few more comments, ...

--- qchem_aimd_tools.py
+++ qchem_aimd_tools.py
@@ -17,6 +17,7 @@ plot_type_choices = (
     'EComponents',
     'Energy',
     'MulMoments',
+    # It doesn't make sense to plot these right now.
     # 'NucCarts',
     # 'NucForces',
     # 'NucVeloc',
@@ -35,6 +36,7 @@ def make_file_iterator(filename):
 
 
 def getargs():
+    """Get and parse command-line arguments."""
 
     import argparse
 
@@ -51,18 +53,19 @@ def getargs():
     parser.add_argument("--normalize-bottom",
                         type=float,
                         default=0.0,
-                        help="""""")
+                        help="""If normalizing data, the minimum value.""")
     parser.add_argument("--normalize-top",
                         type=float,
                         default=1.0,
-                        help="""""")
+                        help="""If normalizing data, the maximum value.""")
 
     args = parser.parse_args()
 
     return args
 
 
-def get_qchem_aimd_data(file_iterator, plot_type, num_header_lines, num_columns, col_types):
+def get_qchem_aimd_data(file_iterator, num_header_lines, num_columns, col_types):
+    """Given a file iterator over a Q-Chem"""
 
     fi = file_iterator
 
@@ -80,14 +83,21 @@ def get_qchem_aimd_data(file_iterator, plot_type, num_header_lines, num_columns,
     return header_lines, data
 
 
-def plot_qchem_aimd_data(column_titles, data, plotfilename, plot_title):
+def plot_qchem_aimd_data(column_titles, data, plotfilename, plot_title, do_normalization=False, norm_bottom=0.0, norm_top=1.0):
+    """Plot a bunch of data from a Q-Chem AIMD file found in
+    `scratchdir/AIMD`.
+
+    Make no assumptions about the first column of data.
+    """
 
     xticks = range(1, len(data) + 1)
 
     fig, ax = plt.subplots()
 
     for idx, coltitle in enumerate(column_titles):
-        dps = normalize_data([tp[idx] for tp in data])
+        dps = [tp[idx] for tp in data]
+        if do_normalization:
+            dps = normalize_data(dps, norm_bottom, norm_top)
         ax.plot(xticks, dps, label=coltitle)
 
     ax.set_title(plot_title)
@@ -97,7 +107,12 @@ def plot_qchem_aimd_data(column_titles, data, plotfilename, plot_title):
     return
 
 
-def plot_qchem_aimd_data_1stcol_time(column_titles, data, plotfilename, plot_title):
+def plot_qchem_aimd_data_1stcol_time(column_titles, data, plotfilename, plot_title, do_normalization=False, norm_bottom=0.0, norm_top=1.0):
+    """Plot a bunch of data from a Q-Chem AIMD file found in
+    `scratchdir/AIMD`.
+
+    Assume that the first column is the time step.
+    """
 
     time = [tp[0] for tp in data]
 
@@ -105,6 +120,8 @@ def plot_qchem_aimd_data_1stcol_time(column_titles, data, plotfilename, plot_tit
 
     for idx, coltitle in enumerate(column_titles, 1):
         dps = normalize_data([tp[idx] for tp in data])
+        if do_normalization:
+            dps = normalize_data(dps, norm_bottom, norm_top)
         ax.plot(time, dps, label=coltitle)
 
     ax.set_title(plot_title)
@@ -128,6 +145,7 @@ def normalize_data(coldata, norm_bottom=0.0, norm_top=1.0):
 
 
 def main(args):
+    """The main routine."""
 
     inputfilename = args.inputfilename
     file_iterator = make_file_iterator(inputfilename)
@@ -143,9 +161,15 @@ def main(args):
             "SCF cycles",
             "CPU time/sec",
         ]
-        header_lines, data = get_qchem_aimd_data(file_iterator, plot_type, num_header_lines, num_columns, col_types)
+        header_lines, data = get_qchem_aimd_data(file_iterator, num_header_lines, num_columns, col_types)
         plotfilename = plot_type + ".pdf"
-        plot_qchem_aimd_data(column_titles, data, plotfilename, plot_title=plot_type)
+        plot_qchem_aimd_data(column_titles,
+                             data,
+                             plotfilename,
+                             plot_title=plot_type,
+                             do_normalization=args.normalize,
+                             norm_bottom=args.normalize_bottom,
+                             norm_top=args.normalize_top)
     elif plot_type == "DipoleACF":
         # Dipole autocorrelation function
         # Time/fs   < u(t) u(0)>     0.00000     0.000000e+00 
@@ -173,7 +197,7 @@ def main(args):
             # Since we're doing AIMD and not ELMD, there aren't any fictious masses.
             # "T(Fict)"
         ]
-        header_lines, data = get_qchem_aimd_data(file_iterator, plot_type, num_header_lines, num_columns, col_types)
+        header_lines, data = get_qchem_aimd_data(file_iterator, num_header_lines, num_columns, col_types)
         plotfilename = plot_type + ".pdf"
         plot_qchem_aimd_data_1stcol_time(column_titles, data, plotfilename, plot_title=plot_type)
     elif plot_type == "Energy":
@@ -185,9 +209,15 @@ def main(args):
             "E(total) - E(previous)",
             "E(total) - E(initial)",
         ]
-        header_lines, data = get_qchem_aimd_data(file_iterator, plot_type, num_header_lines, num_columns, col_types)
+        header_lines, data = get_qchem_aimd_data(file_iterator, num_header_lines, num_columns, col_types)
         plotfilename = plot_type + ".pdf"
-        plot_qchem_aimd_data_1stcol_time(column_titles, data, plotfilename, plot_title=plot_type)
+        plot_qchem_aimd_data_1stcol_time(column_titles,
+                                         data,
+                                         plotfilename,
+                                         plot_title=plot_type,
+                                         do_normalization=args.normalize,
+                                         norm_bottom=args.normalize_bottom,
+                                         norm_top=args.normalize_top)
     elif plot_type == "MulMoments":
         # Time/fs  Dipole + higher multipole moments (Debye*Angst**(L-1))
         num_header_lines = 1
@@ -198,9 +228,16 @@ def main(args):
             "dipole (Y)",
             "dipole (Z)",
         ]
-        header_lines, data = get_qchem_aimd_data(file_iterator, plot_type, num_header_lines, num_columns, col_types)
+        header_lines, data = get_qchem_aimd_data(file_iterator, num_header_lines, num_columns, col_types)
         plotfilename = plot_type + ".pdf"
-        plot_qchem_aimd_data_1stcol_time(column_titles, data, plotfilename, plot_title=plot_type)
+        plot_qchem_aimd_data_1stcol_time(column_titles,
+                                         data,
+                                         plotfilename,
+                                         plot_title=plot_type,
+                                         do_normalization=args.normalize,
+                                         norm_bottom=args.normalize_bottom,
+                                         norm_top=args.normalize_top)
+
     # elif plot_type == "NucCarts":
     #     pass
     # elif plot_type == "NucForces":
@@ -218,9 +255,15 @@ def main(args):
         ]
         # Because of a bug in the output, do some trickery...
         next(file_iterator)
-        header_lines, data = get_qchem_aimd_data(file_iterator, plot_type, num_header_lines, num_columns, col_types)
+        header_lines, data = get_qchem_aimd_data(file_iterator, num_header_lines, num_columns, col_types)
         plotfilename = plot_type + ".pdf"
-        plot_qchem_aimd_data_1stcol_time(column_titles, data, plotfilename, plot_title=plot_type)
+        plot_qchem_aimd_data_1stcol_time(column_titles,
+                                         data,
+                                         plotfilename,
+                                         plot_title=plot_type,
+                                         do_normalization=args.normalize,
+                                         norm_bottom=args.normalize_bottom,
+                                         norm_top=args.normalize_top)
     elif plot_type == "TandV":
         # Time/fs  V(total)  T(total)  V(t)-V(0)  T(t)-T(0)
         num_header_lines = 1
@@ -232,9 +275,15 @@ def main(args):
             "V(t) - V(0)",
             "T(t) - T(0)",
         ]
-        header_lines, data = get_qchem_aimd_data(file_iterator, plot_type, num_header_lines, num_columns, col_types)
+        header_lines, data = get_qchem_aimd_data(file_iterator, num_header_lines, num_columns, col_types)
         plotfilename = plot_type + ".pdf"
-        plot_qchem_aimd_data_1stcol_time(column_titles, data, plotfilename, plot_title=plot_type)
+        plot_qchem_aimd_data_1stcol_time(column_titles,
+                                         data,
+                                         plotfilename,
+                                         plot_title=plot_type,
+                                         do_normalization=args.normalize,
+                                         norm_bottom=args.normalize_bottom,
+                                         norm_top=args.normalize_top)
     elif plot_type == "Torque":
         # Time/fs, Torque, angular momentum, c.o.m. translational force
         num_header_lines = 1
@@ -251,10 +300,17 @@ def main(args):
             "c.o.m. translational force (Y)",
             "c.o.m. translational force (Z)",
         ]
-        header_lines, data = get_qchem_aimd_data(file_iterator, plot_type, num_header_lines, num_columns, col_types)
+        header_lines, data = get_qchem_aimd_data(file_iterator, num_header_lines, num_columns, col_types)
         plotfilename = plot_type + ".pdf"
-        plot_qchem_aimd_data_1stcol_time(column_titles, data, plotfilename, plot_title=plot_type)
+        plot_qchem_aimd_data_1stcol_time(column_titles,
+                                         data,
+                                         plotfilename,
+                                         plot_title=plot_type,
+                                         do_normalization=args.normalize,
+                                         norm_bottom=args.normalize_bottom,
+                                         norm_top=args.normalize_top)
     else:
+        # Meh...
         pass
 
     # The data we retrieve from Q-Chem AIMD single outputs is such
