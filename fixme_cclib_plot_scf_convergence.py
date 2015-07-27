08d60973e23a4c2e4b339ca0a96b42e139d6ed6c
Eric Berquist <erb74@pitt.edu>
2015-07-27 08:41:48 -0400

    same

---
+++ fixme_cclib_plot_scf_convergence.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+"""cclib_plot_scf_convergence.py: Given a computational chemistry
+logfile for ...
+"""
+
+from __future__ import print_function
+
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+
+import argparse
+import os.path
+
+import cclib
+from cclib.parser import ccopen
+from cclib.parser import utils
+
+
+def main():
+    """The main routine!"""
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('compchemfilename', nargs='+')
+
+    args = parser.parse_args()
+    compchemfilenames = args.compchemfilename
+
+    for compchemfilename in compchemfilenames:
+
+        stub = os.path.splitext(compchemfilename)[0]
+
+        job = ccopen(compchemfilename)
+        data = job.parse()
+
+        fig, ax = plt.subplots()
+
+        if type(job) == cclib.parser.qchemparser.QChem:
+
+            scfenergies = [utils.convertor(scfenergy, 'eV', 'hartree') for scfenergy in data.scfenergies]
+            print(scfenergies)
+            # scfenergies = [scfenergy for scfenergy in data.scfenergies]
+
+            steps = range(1, len(scfenergies) + 1)
+
+            ax.plot(steps, scfenergies, label='SCF energy')
+
+            ax.set_title(stub)
+            ax.set_xlabel('SCF step #')
+            ax.set_xticks(steps)
+
+        elif type(job) == cclib.parser.orcaparser.ORCA:
+
+            pass
+
+        else:
+            pass
+
+        ax.legend(loc='best', fancybox=True)
+
+        fig.savefig(stub + '.pdf', bbox_inches='tight')
+
+
+if __name__ == "__main__":
+    main()
