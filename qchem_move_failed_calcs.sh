066fdf6cabe6a3bc3242d97d1dcc5c4bfc728eba
Eric Berquist <erb74@pitt.edu>
2015-08-31 10:40:55 -0400

    Unify a few things

---
+++ qchem_move_failed_calcs.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# find_failed_calcs.sh: Find incomplete or file Q-Chem calculations
+# and move all files related to the calculation to a separate folder.
+
+destdir=incomplete
+mkdir -p "${destdir}"
+
+outputs=$(find . -type f -name "*.out")
+
+# This is how many times the search string needs to show up for a
+# calculation to be considered "completed".
+count_for_completed="${1}"
+searchstr='Thank you very much for using Q-Chem'
+
+for output in ${outputs[@]}; do
+    count=$(grep -c "${searchstr}" "${output}")
+    if [ "${count}" -ne "${count_for_completed}" ]; then
+        stub="${output%.*}"
+        mv -v ${stub}.* "${destdir}"
+    fi
+done
