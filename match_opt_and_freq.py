ff11f8baef05fd3a497b27ddd5744d911f8db4dd
Eric Berquist <eric.berquist@gmail.com>
2015-11-06 08:06:39 -0500

    Move file-finding with extensions to another function.

--- match_opt_and_freq.py
+++ match_opt_and_freq.py
@@ -14,6 +14,9 @@ def getargs():
 
     parser = argparse.ArgumentParser()
 
+    parser.add_argument('--debug', action='store_true')
+    parser.add_argument('--debug-big', action='store_true')
+
     parser.add_argument('--dir-opt', default='.')
     parser.add_argument('--dir-freq', default='.')
 
@@ -29,28 +32,58 @@ def getargs():
     return args
 
 
+def pprint(l, header):
+    print("========== {}: ==========".format(header))
+    for x in l:
+        print(x)
+    return
+
+
+def get_files_from_path(path, ext=".out"):
+    matchfiles = []
+    # Walk the directory tree to find all potential output files.
+    for (root, dirs, files) in os.walk(path):
+        for f in files:
+            if f.endswith(ext):
+                matchfiles.append(os.path.join(root, f))
+    return sorted(matchfiles)
+
+
 if __name__ == '__main__':
+
+    import sys
 
     args = getargs()
 
     dir_opt = os.path.abspath(args.dir_opt)
     dir_freq = os.path.abspath(args.dir_freq)
 
-    inputs_opt = [os.path.basename(f) for f in os.listdir(path=dir_opt)
-                  if f.endswith(".in") and "opt" in os.path.basename(f)]
-    inputs_freq = [os.path.basename(f) for f in os.listdir(path=dir_freq)
-                   if f.endswith(".in") and "freq" in os.path.basename(f)]
-    outputs_opt = [os.path.basename(f) for f in os.listdir(path=dir_opt)
-                   if f.endswith(".out") and "opt" in os.path.basename(f)]
-    outputs_freq = [os.path.basename(f) for f in os.listdir(path=dir_freq)
-                    if f.endswith(".out") and "freq" in os.path.basename(f)]
+    if args.debug:
+        print("dir_opt : {}".format(dir_opt), file=sys.stderr)
+        print("dir_freq: {}".format(dir_freq), file=sys.stderr)
 
+    inputs_opt = [f for f in get_files_from_path(dir_opt, ext=".in")
+                  if "opt" in os.path.basename(f)]
+    inputs_freq = [f for f in get_files_from_path(dir_freq, ext=".in")
+                   if "freq" in os.path.basename(f)]
+    outputs_opt = [f for f in get_files_from_path(dir_opt, ext=".out")
+                   if "opt" in os.path.basename(f)]
+    outputs_freq = [f for f in get_files_from_path(dir_freq, ext=".out")
+                    if "freq" in os.path.basename(f)]
+
+    if args.debug_big:
+        pprint(inputs_opt, "inputs_opt")
+        pprint(inputs_freq, "inputs_freq")
+        pprint(outputs_opt, "outputs_opt")
+        pprint(outputs_freq, "outputs_freq")
+
+    # Master loop over all completed geometry optimization output
+    # files.
     for output_opt in outputs_opt:
-        output_opt_bn = os.path.basename(output_opt)
-        opt_repl_freq = re.sub('opt\d*', 'freq', output_opt_bn)
+        opt_repl_freq = os.path.basename(re.sub('opt\d*', 'freq', output_opt))
         if args.print_opt_outputs_missing_freq_outputs:
             if opt_repl_freq not in outputs_freq:
-                print(output_opt_bn)
+                print(os.path.relpath(output_opt))
         if args.print_opt_outputs_missing_freq_inputs:
             if opt_repl_freq.replace(".out", ".in") not in inputs_freq:
-                print(output_opt_bn)
+                print(os.path.relpath(output_opt))
