9f78d6bd0839d6b0f3314ce891ca3b96b442f6d2
Eric Berquist <eric.berquist@gmail.com>
2015-11-06 08:04:33 -0500

    It makes more sense for shell pipelining to echo input files.

--- match_inputs_and_outputs.sh
+++ match_inputs_and_outputs.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
 # match_inputs_and_outputs.sh: For each input file, is there a
-# matching output file? Print out names of queue submission files that
-# would create missing outputs.
+# matching output file? Print out names of input files that would
+# create missing outputs.
 
 inputfiles=$(ls *.in)
 
 for inputfile in ${inputfiles[@]}; do
     outputfile=${inputfile//\.in/\.out}
-    pbsfile=${inputfile//\.in/\.pbs}
     if [ ! -e ${outputfile} ]; then
-        echo ${pbsfile}
+        echo ${inputfile}
     fi
 done
