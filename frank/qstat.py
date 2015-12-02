a28138b82c0b66c5087b16618d6e0c1c62f2fd52
Eric Berquist <eric.berquist@gmail.com>
2015-12-02 12:04:49 -0500

    Start making queue utility functions.

--- frank/qstat.py
+++ frank/qstat.py
@@ -2,28 +2,13 @@
 
 from __future__ import print_function
 
-import re
-import subprocess as sp
 import sys
 
+from queue_utils import getargs
 from queue_utils import make_spaced_dashes
-
-
-def getargs():
-
-    import argparse
-
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('-u',
-                        dest='username',
-                        nargs='+',
-                        help="""""")
-
-    args = parser.parse_args()
-
-    return args
-
+from queue_utils import get_qstat_short_output
+from queue_utils import get_qstat_full_outputs
+from queue_utils import parse_qstat_full_output
 
 
 if __name__ == '__main__':
@@ -31,60 +16,26 @@ if __name__ == '__main__':
     args = getargs()
     usernames = args.username
 
-    if usernames:
-        if len(usernames) == 1:
-            # This will capture both a single usename and the typical
-            # comma-separated list without spaces.
-            qstat_cmd = ['qstat', '-u', usernames[0]]
-        else:
-            qstat_cmd = ['qstat', '-u', ','.join(usernames)]
-    else:
-        qstat_cmd = ['qstat', '-a']
-
-    short_output = sp.check_output(qstat_cmd).decode('utf-8').splitlines()
-    short_output = short_output[5:]
-
-    re_ints = re.compile('\d*')
-
     blank_spot = '--'
 
     reformatted_lines = []
 
-    for idx, short_output_line in enumerate(short_output):
+    short_output = get_qstat_short_output(usernames)
+    full_outputs = get_qstat_full_outputs(short_output)
 
-        jobinfo = dict()
-        short_output_line_chomp = short_output_line.split()
-        job = re_ints.match(short_output_line_chomp[0])
-        jobid = job.group()
+    for full_output in full_outputs:
 
-        try:
-            full_output = sp.check_output(['qstat', '-f', str(jobid)]).decode('utf-8')
-        except sp.CalledProcessError as e:
-            continue
+        jobinfo = parse_qstat_full_output(full_output, debug=args.debug)
 
-        for full_output_line in full_output.splitlines():
-
-            chomp = full_output_line.split()
-
-            if 'Id:' in chomp:
-                pass
-            elif len(chomp) > 2:
-                key = chomp[0]
-                value = ' '.join(chomp[2:])
-                jobinfo[key] = value
-            else:
-                oldvalue = jobinfo[key]
-                newvalue = ''.join([full_output_line, oldvalue])
-                jobinfo[key] = newvalue
-
+        jobid = jobinfo['Job'].split('.')[0]
         username = jobinfo['euser']
         group = jobinfo['egroup']
         queue = jobinfo['queue']
         jobname = jobinfo['Job_Name']
         session_id = jobinfo.get('session_id', blank_spot)
-        # nodes = jobinfo['Resource_List.nodect']
-        nodes = short_output_line_chomp[5]
-        tasks = short_output_line_chomp[6]
+        nodes = jobinfo['Resource_List.nodect']
+        # tasks = short_output_line_chomp[6]
+        tasks = jobinfo['Resource_List.nodes'].split('=')[-1]
         reqd_mem = jobinfo.get('Resource_List.pmem', blank_spot)
         reqd_time = jobinfo['Resource_List.walltime']
         status = jobinfo['job_state']
---
+++ frank/queue_get_output_path.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import os.path
+
+from queue_utils import getargs
+from queue_utils import get_qstat_short_output
+from queue_utils import get_qstat_full_outputs
+from queue_utils import parse_qstat_full_output
+
+
+if __name__ == '__main__':
+
+    args = getargs()
+
+    short_output = get_qstat_short_output(args.username)
+    full_outputs = get_qstat_full_outputs(short_output)
+    jobinfos = [parse_qstat_full_output(full_output)
+                for full_output in full_outputs]
+
+    for jobinfo in jobinfos:
+        assert 'Output_Path' in jobinfo.keys()
+        output_path = os.path.dirname(jobinfo['Output_Path'].split(':')[1])
+        print(output_path)
