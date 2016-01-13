f9251c293230ba8b8b546a7a12629ac5a8fbca23
Eric Berquist <eric.berquist@gmail.com>
2016-01-13 10:26:10 -0500

    When extracting the last possible XYZ coordinates, dump a Q-Chem fragment file is possible.
    This should be cleaned up...

--- cclib_extract_last_geom.py
+++ cclib_extract_last_geom.py
@@ -9,6 +9,7 @@ cclib. Name is the same stub, with the file extension replaced by
 from __future__ import print_function
 import argparse
 import os.path
+import cclib
 from cclib.parser import ccopen
 from cclib.parser.utils import PeriodicTable
 
@@ -51,5 +52,53 @@ for qmoutfile in qmoutfiles:
         for atom, atomcoords in zip(element_list, last_geometry):
             # pylint: disable=W0142
             xyzfile.write(s.format(atom, *atomcoords) + '\n')
+        print(xyzfilename)
 
-    print(xyzfilename)
+    # If this is from a Q-Chem fragment calculation, print a single
+    # fragment "XYZ" file as well.
+    if isinstance(job, cclib.parser.qchemparser.QChem):
+        with open(qmoutfile) as fh:
+            charges = []
+            multiplicities = []
+            start_indices = []
+            line = ''
+            while '$molecule' not in line:
+                line = next(fh)
+            line = next(fh)
+            sys_charge, sys_multiplicity = line.split()
+            counter = -1
+            # Gather the charges, spin multiplicities, and starting
+            # positions of each fragment.
+            while '$end' not in line:
+                if '--' in line:
+                    line = next(fh)
+                    charge, multiplicity = line.split()
+                    charges.append(charge)
+                    multiplicities.append(multiplicity)
+                    start_indices.append(counter)
+                    line = next(fh)
+                else:
+                    counter += 1
+                    line = next(fh)
+        assert len(charges) == len(multiplicities) == len(start_indices)
+        if suffix:
+            fragxyzfilename = ''.join([stub, '.', suffix, '.xyz_frag'])
+        else:
+            fragxyzfilename = ''.join([stub, '.xyz_frag'])
+        with open(fragxyzfilename, 'w') as fh:
+            t = '{:3} {:15.10f} {:15.10f} {:15.10f}'.format
+            blocks = []
+            blocks.append('{} {}'.format(sys_charge, sys_multiplicity))
+            from itertools import count
+            for (charge, multiplicity, idx_iter) in zip(charges, multiplicities, count(0)):
+                blocks.append('--')
+                blocks.append('{} {}'.format(charge, multiplicity))
+                idx_start = start_indices[idx_iter]
+                try:
+                    idx_end = start_indices[idx_iter + 1]
+                except IndexError:
+                    idx_end = len(element_list)
+                for atomsym, atomcoords in zip(element_list[idx_start:idx_end], last_geometry[idx_start:idx_end]):
+                    blocks.append(t(atomsym, *atomcoords))
+            fh.write('\n'.join(blocks))
+            print(fragxyzfilename)
