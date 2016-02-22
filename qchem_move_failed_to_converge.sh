0453862b21912c795d2628de89c78c65a30379ee
Eric Berquist <eric.berquist@gmail.com>
2016-02-22 13:41:34 -0500

    Q-Chem: simple tools for moving files around.

---
+++ qchem_move_failed_to_converge.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# qchem_move_failed_to_converge.sh: Find Q-Chem calculations that
+# failed to converge the energy and move all files related to the
+# calculation to a separate folder.
+
+destdir="${1}"
+mkdir -p "${destdir}"
+
+# assume everything is an Q-Chem output!
+outputs=$(find . -maxdepth 1 -type f -name "*.out")
+
+# This signifies the calculation has finished, but the SCF didn't
+# converge.
+searchstr='SCF failed to converge'
+
+for output in ${outputs[@]}; do
+    count=$(tail "${output}" | grep -c "${searchstr}")
+    if [ "${count}" -eq 1 ]; then
+        stub="${output%.*}"
+        mv -v ${stub}.* "${destdir}"
+    fi
+done
---
+++ qchem_move_incomplete_geoms.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# qchem_move_incomplete_geoms.sh: Find incomplete Q-Chem geometry
+# optimizations that otherwise terminated normally (just ran out of
+# cycles) and move all files related to the calculation to a separate
+# folder.
+
+destdir="${1}"
+mkdir -p "${destdir}"
+
+# assume everything is an Q-Chem output!
+outputs=$(find . -maxdepth 1 -type f -name "*.out")
+
+# This signifies the calculation has finished, but the geometry
+# optimization didn't converge in time.
+searchstr='MAXIMUM OPTIMIZATION CYCLES REACHED'
+
+for output in ${outputs[@]}; do
+    count=$(tail "${output}" | grep -c "${searchstr}")
+    if [ "${count}" -eq 1 ]; then
+        stub="${output%.*}"
+        mv -v ${stub}.* "${destdir}"
+    fi
+done
