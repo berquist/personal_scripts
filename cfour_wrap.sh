6cdc18df1759c8bbb72147f0d7c1012db9909320
Eric Berquist <eric.berquist@gmail.com>
2016-02-29 11:00:18 -0500

    handle annoying file operations when running CFOUR

--- cfour_wrap.sh
+++ cfour_wrap.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-# cfour_wrap.sh: 
+# cfour_wrap.sh: Handle some of the dirty stuff when running a CFOUR
+# calculation.
 
 # Strip off the file extension to get the root name of the
 # calculation.
 stub=${1%.*}
 ext=${1##*.}
 
-# CFOUR needs to run in its own directory.
+# CFOUR needs to run in its own directory. Make sure it doesn't exist
+# before running.
+rm -r ${stub}
 mkdir ${stub}
 cd ${stub}
 cp ../${stub}.${ext} ./ZMAT
 ln -s ${CFOUR_ROOT}/basis/GENBAS .
 xcfour > ${stub}.out
+cp ${stub}.out ..
 cd ..
