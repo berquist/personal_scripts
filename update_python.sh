9e23e5c9852e82e40ed1ee2db41dfe0e8dda6604
Eric Berquist <eric.berquist@gmail.com>
2016-02-29 10:39:05 -0500

    update python

--- update_python.sh
+++ update_python.sh
@@ -1,19 +1,11 @@
 #!/usr/bin/env bash
 
 module purge
-module load python/anaconda
-conda update --yes conda
-conda update --yes anaconda
-conda update --yes accelerate
-conda clean --yes --tarballs
-conda clean --yes --packages
-conda clean --yes --lock
+module load python/anaconda2
+conda update --yes --all
+conda clean --yes -i -l -t -p -s
 
 module purge
 module load python/anaconda3
-conda update --yes conda
-conda update --yes anaconda
-conda update --yes accelerate
-conda clean --yes --tarballs
-conda clean --yes --packages
-conda clean --yes --lock
+conda update --yes --all
+conda clean --yes -i -l -t -p -s
