2afd2b5491d89793ebfcee2eb393c00bc0c4aedc
Eric Berquist <eric.berquist@gmail.com>
2016-04-25 14:21:11 -0400

    Move Psi4 completed calculations.

---
+++ psi4_move_completed_calcs.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# psi4_move_completed_calcs.sh: Move completed calculations (both
+# inputs and outputs) to the specified folder (second argument).
+
+destdir="${2}"
+mkdir -p "${destdir}"
+
+outputs=$(find . -maxdepth 1 -type f -name "*.out")
+
+# This is how many times the search string needs to show up for a
+# calculation to be considered "completed".
+count_for_completed="${1}"
+searchstr='PSI4 exiting successfully.'
+
+for output in ${outputs[@]}; do
+    count=$(grep -c "${searchstr}" "${output}")
+    if [ "${count}" -eq "${count_for_completed}" ]; then
+        stub="${output%.*}"
+        mv -v ${stub}.* "${destdir}"
+    fi
+done
