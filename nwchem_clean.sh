70a6696988796ac512c0f6d2c1a519ae320fc19f
Eric Berquist <eric.berquist@gmail.com>
2016-08-03 10:37:06 -0400

    update NWChem cleaner

--- nwchem_clean.sh
+++ nwchem_clean.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-set -v
-
 rm -f ./*.b
 # rm -f "./*.b^-1"
 rm -f ./*.c
@@ -11,7 +9,13 @@ rm -f ./*.cphf_sol.*
 rm -f ./*.db
 rm -f ./*.gridpts.*
 rm -f ./*.grinfo.*
-rm -f ./*.movecs
+rm -f ./*.lagr
+rm -f ./*.movec*
 rm -f ./*.nmode
 rm -f ./*.p
+rm -f ./*.shieldcphf
 rm -f ./*.zmat
+rm -f ./*.zora_gshiftcphf
+rm -f ./*.zora_nmrgshift
+rm -f ./*.zora_nmrgshift_AB
+rm -f ./*.zora_sf
