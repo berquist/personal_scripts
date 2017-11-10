3087b350a3ae6823035dd3a165bbb97b102063da
Eric Berquist <eric.berquist@gmail.com>
2017-11-10 10:40:46 -0500

    SLURM script for Psi4

---
+++ clusters/haswell/submit_psi4.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+"""submit_psi4.py: A standalone script for submitting Psi4 jobs to
+Haswell's SLURM scheduler.
+"""
+
+from __future__ import print_function
+
+
+def template_slurmfile_psi4(inpfile, ppn, time, as_python):
+    """The template for a SLURM jobfile that calls Psi4."""
+    command = '$(which psi4) -n $SLURM_NTASKS_PER_NODE "{inpfile}.in"'.format(inpfile=inpfile)
+    if as_python:
+        command = 'OMP_NUM_THREADS={ppn} $(which python) "{inpfile}.py"'.format(inpfile=inpfile)
+    return '''#!/bin/bash
+
+#SBATCH --job-name={inpfile}
+#SBATCH --output={inpfile}.slurmout
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node={ppn}
+#SBATCH --time=0-{time}:00:00
+#SBATCH --partition=smp
+
+module purge
+module load python/anaconda3
+
+{command}
+chmod 644 "$SLURM_SUBMIT_DIR/{inpfile}.out"
+'''.format(command=command,
+           inpfile=inpfile,
+           ppn=ppn,
+           time=time)
+
+if __name__ == '__main__':
+    import argparse
+    import os.path
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('inpfilename',
+                        help='the input file to submit')
+    parser.add_argument('--ppn',
+                        type=int,
+                        default=12,
+                        help='number of cores to run on (max 12)')
+    parser.add_argument('--time',
+                        type=int,
+                        default=24,
+                        help='walltime to reserve (max 144 hours)')
+    parser.add_argument('--as-python',
+                        action='store_true',
+                        help='Is the input file actually a Python script?')
+    args = parser.parse_args()
+    inpfilename = os.path.splitext(args.inpfilename)[0]
+
+    slurmfilename = inpfilename + '.slurm'
+    with open(slurmfilename, 'w') as slurmfile:
+        slurmfile.write(template_slurmfile_psi4(inpfilename,
+                                                 args.ppn,
+                                                 args.time))
+    print(slurmfilename)
