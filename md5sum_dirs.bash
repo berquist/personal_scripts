6cf68e0e89779e2e5af267009611f541efde9ce6
Eric Berquist <eric.berquist@gmail.com>
2018-11-03 18:14:06 -0400

    run from any path and clean up printing

--- md5sum_dirs.bash
+++ md5sum_dirs.bash
@@ -2,10 +2,11 @@
 
 hashfunc=md5sum
 
-directories=$(printf "%s\n" "$@" | ./basename_sort.pl)
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+directories=$(printf "%s\n" "$@" | "$SCRIPTDIR"/basename_sort.pl)
 echo "$directories"
 
 for d in $directories; do
     hash=$(find "$d" -type f -not -path '*/\.git/*' -exec $hashfunc {} \; | sort -k 2 | $hashfunc)
-    printf "%s %s\n" "$hash" "$d"
+    printf "%s %s\n" "$(echo "$hash" | cut -d ' ' -f 1)" "$d"
 done
