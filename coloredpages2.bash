1c62cd5c69cd98cdc6e9594c8631ac96dd113473
Eric Berquist <eric.berquist@gmail.com>
2018-11-23 17:02:45 -0500

    two ways of calculating how many colored pages are in a PDF

---
+++ coloredpages2.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# https://superuser.com/questions/234408/count-bw-color-pages-in-pdf#comment1874045_721773
+
+file="$1"
+
+gs -o - -sDEVICE=inkcov "$file" | tail -n +4 | sed '/^Page*/N;s/\n//' | sed -E '/Page [0-9]* 0.00000  0.00000  0.00000/d'
