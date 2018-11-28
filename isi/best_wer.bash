a6996e2050550bbf01719b518e94fe886cb69724
Eric Berquist <berquist@isi.edu>
2018-11-28 08:06:07 -0800

    be more general when looking for decode directories

--- isi/best_wer.bash
+++ isi/best_wer.bash
@@ -3,6 +3,6 @@
 BEST_WER=/nas/material/users/jbilla/eesen/asr_egs/wsj/utils/best_wer.sh
 
 # find . -type d -name "decode_analysis1*" 2>/dev/null
-(for i in train_e2e/decode_analysis1_*; do
+(for i in decode*analysis1*; do
      grep WER $i/wer_* | ${BEST_WER};
  done) | sort -k2,2
