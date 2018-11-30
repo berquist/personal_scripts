3a034cbb14e2b39506cc7b06e6fbd2e91f0ad747
Eric Berquist <berquist@isi.edu>
2018-11-30 14:32:41 -0800

    Handle scp files that may not contain offsets

--- isi/diff_scp.py
+++ isi/diff_scp.py
@@ -2,6 +2,7 @@
 
 import difflib
 from pathlib import Path
+from typing import List, Tuple
 
 
 def getargs():
@@ -13,12 +14,17 @@ def getargs():
     return parser.parse_args()
 
 
-def get_scp_lines(scp_filename):
+def get_scp_lines(scp_filename) -> Tuple[List[str], List[str]]:
     with open(scp_filename) as handle:
         lines = handle.readlines()
     splitlines = [line.split() for line in lines]
+    # Try and extract IDs and offsets from the file; offsets may not be
+    # available.
     ids = [splitline[0] for splitline in splitlines]
-    offsets = [splitline[1].split(":")[1] for splitline in splitlines]
+    try:
+        offsets = [splitline[1].split(":")[1] for splitline in splitlines]
+    except IndexError:
+        offsets = []
     return ids, offsets
     
 
@@ -29,12 +35,13 @@ def diff_scp_lines(filename_scp1, filename_scp2):
     ids2, offsets2 = get_scp_lines(filename_scp2)
 
     diff_ids = list(difflib.unified_diff(ids1, ids2))
-    diff_offsets = list(difflib.unified_diff(offsets1, offsets2))
+    diff_offsets = []
+    if offsets1 and offsets2:
+        diff_offsets = list(difflib.unified_diff(offsets1, offsets2))
 
     if diff_ids or diff_offsets:
         return False
-    else:
-        return True
+    return True
 
 
 if __name__ == "__main__":
