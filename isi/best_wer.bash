ed5d4a729351ea4bbbb28c4a9cf8956608943f87
Eric Berquist <berquist@isi.edu>
2019-02-21 09:10:15 -0800

    WER is calculated for all analysis sets

--- isi/best_wer.bash
+++ isi/best_wer.bash
@@ -2,7 +2,6 @@
 
 BEST_WER=/nas/home/berquist/opt/apps/build/kaldi/egs/wsj/s5/utils/best_wer.sh
 
-# find . -type d -name "decode_analysis1*" 2>/dev/null
-(for i in decode*analysis1*; do
+(for i in decode*analysis*; do
      grep WER $i/wer_* | ${BEST_WER};
  done) | sort -k2,2
