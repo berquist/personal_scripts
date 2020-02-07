0cd5033193f8cca5f335a0e38d1f521d13732aee
Eric Berquist <eric.berquist@gmail.com>
2020-02-06 23:15:56 -0500

    blacken

--- qchem_total_time.py
+++ qchem_total_time.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python
 
 import numpy as np
-np_formatter = {
-    'float_kind': lambda x: '{:14.8f}'.format(x)
-}
+
+np_formatter = {"float_kind": lambda x: "{:14.8f}".format(x)}
 np.set_printoptions(linewidth=200, formatter=np_formatter)
+
 
 def qchem_get_total_times(outputfilename):
     time_wall, time_cpu = None, None
     with open(outputfilename) as fh:
         for line in fh:
-            if 'Total job time' in line:
+            if "Total job time" in line:
                 tokens = line.split()
                 time_wall_str, time_cpu_str = tokens[3], tokens[4]
                 time_wall = float(time_wall_str[:-8])
                 time_cpu = float(time_cpu_str[:-6])
     return time_wall, time_cpu
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 
     import argparse
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('outputfile', nargs='+')
+    parser.add_argument("outputfile", nargs="+")
 
     args = parser.parse_args()
 
@@ -42,13 +43,17 @@ if __name__ == '__main__':
     times_hours_wall = times_hours[:, 0]
     times_hours_cpu = times_hours[:, 1]
     speedup = times_hours_cpu / times_hours_wall
-    print('times (hours):')
-    print('wall (sorted by) | cpu')
+    print("times (hours):")
+    print("wall (sorted by) | cpu")
     print(times_hours)
-    print('speedup:')
+    print("speedup:")
     print(speedup)
     mmin, mmax = min(times_hours_wall), max(times_hours_wall)
     rng = mmax - mmin
-    print('min, max, range:')
+    print("min, max, range:")
     print(mmin, mmax, rng)
-    print('mean +/- stdev: {:f} +/- {:f}'.format(np.mean(times_hours_wall), np.std(times_hours_wall)))
+    print(
+        "mean +/- stdev: {:f} +/- {:f}".format(
+            np.mean(times_hours_wall), np.std(times_hours_wall)
+        )
+    )
