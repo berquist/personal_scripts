c2f7c8b7fd5f42984b062819185b6fe03822faad
Eric Berquist <eric.berquist@gmail.com>
2020-06-20 16:17:57 -0400

    No need to force redownload of all repository indices

--- arch_setup_chroot.bash
+++ arch_setup_chroot.bash
@@ -3,7 +3,7 @@
 export CHROOT=/home/eric/data/chroot
 mkdir -p $CHROOT
 mkarchroot $CHROOT/root base-devel
-arch-nspawn $CHROOT/root pacman -Syyu
+arch-nspawn $CHROOT/root pacman -Syu
 
 # Then, to build a package in the chroot, run the following from the
 # dir containing the PKGBUILD:
