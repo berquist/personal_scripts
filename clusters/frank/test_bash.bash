3b5d66a6590f5edff0da85079f269f0973fb2dd6
Eric Berquist <eric.berquist@gmail.com>
2020-12-24 14:03:25 -0500

    remove obsolete scripts

--- clusters/frank/test_bash.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo $1
-echo $2
-echo $3
-
-# $ bashtest.sh herp derp twerp prints herp\nderp\ntwerp
--- clusters/frank/test_write.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-test_file = open('test_file','a')
-max_iter = 100
-for i in range(max_iter):
-    for j in range(max_iter):
-        test_file.write('a[i][j] = a[%i][%i] \n' % (i,j))
