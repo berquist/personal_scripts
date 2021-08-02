5728ce35deeb93b56382cf6e5ee73a72fb5f3272
Eric Berquist <eric.berquist@gmail.com>
2021-08-02 08:32:34 -0400

    remove unused files

--- clusters/frank/frank_shell_switch_to_custom.bash
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-rm ${HOME}/.bash_profile
-rm ${HOME}/.bashrc
-ln -sv ${HOME}/dotfiles/.bash_profile.frank ${HOME}/.bash_profile
-ln -sv ${HOME}/dotfiles/.bashrc.frank ${HOME}/.bashrc
