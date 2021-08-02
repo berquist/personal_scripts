ba3629f4d44415e69f6b16769e04833df378b5b2
Eric Berquist <eric.berquist@gmail.com>
2021-08-02 08:43:47 -0400

    remove unused files

--- chemistry/inscribe_atoms.py
+++ chemistry/inscribe_atoms.py
@@ -8,12 +8,12 @@ import numpy as np
 import itertools
 
 parser = ap.ArgumentParser()
-parser.add_argument('namein', metavar='<input XYZ file>')
+parser.add_argument("namein", metavar="<input XYZ file>")
 args = parser.parse_args()
 # namein = args.namein
 locals().update(vars(args))
 
-handle = open(namein, 'r')
+handle = open(namein, "r")
 contents = handle.readlines()
 
 n = int(contents[0].strip())
@@ -25,13 +25,13 @@ atoms = [atom.strip().split() for atom in contents[2:]]
 maxdist = 0.0
 origin = np.empty(3)
 for i in range(0, n):
-    for j in range(i+1, n):
+    for j in range(i + 1, n):
         ivec = np.array([float(atoms[i][1]), float(atoms[i][2]), float(atoms[i][3])])
         jvec = np.array([float(atoms[j][1]), float(atoms[j][2]), float(atoms[j][3])])
         dist = np.linalg.norm(ivec - jvec)
         if dist > maxdist:
             maxdist = dist
-            origin = (ivec + jvec)/2
+            origin = (ivec + jvec) / 2
 
-print("radius:", maxdist/2)
+print("radius:", maxdist / 2)
 print("origin:", origin)
