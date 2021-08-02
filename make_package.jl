9ca12d3687928f8717550935cfa79e12792d23cf
Eric Berquist <eric.berquist@gmail.com>
2021-08-02 08:20:34 -0400

    personal template for making Julia packages

---
+++ make_package.jl
@@ -0,0 +1,13 @@
+using PkgTemplates
+
+t = Template(;
+             dir="~/development/julia",
+             plugins=[
+                 License(; name="BSD3"),
+                 Git(; ssh=true),
+                 GitHubActions(),
+                 Codecov(),
+                 Coveralls(),
+                 Documenter{GitHubActions}(),
+             ],
+             )
