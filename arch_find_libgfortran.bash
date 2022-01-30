ee911a59e2cbc1931e8bb574d14f0fed0f46a00c
Eric Berquist <eric.berquist@gmail.com>
2022-01-30 14:50:46 -0500

    cleaning

---
+++ arch_find_libgfortran.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+while read pkg; do
+    mapfile -t files < <(pacman -Qlq $pkg | grep -v /$)
+    grep -Fq libgfortran.so.3 "${files[@]}" <&- 2>/dev/null && echo $pkg
+done < <(pacman -Qqm)
--- arch_setup_chroot.bash
+++ arch_setup_chroot.bash
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-export CHROOT=/home/eric/data/chroot
-mkdir -p $CHROOT
-mkarchroot $CHROOT/root base-devel
-arch-nspawn $CHROOT/root pacman -Syu
+export CHROOT="$HOME"/data/chroot
+mkdir -p "$CHROOT"
+mkarchroot "$CHROOT"/root base-devel
+arch-nspawn "$CHROOT"/root pacman -Syu
 
 # Then, to build a package in the chroot, run the following from the
 # dir containing the PKGBUILD:
