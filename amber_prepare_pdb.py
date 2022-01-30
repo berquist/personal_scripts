7a4caca03ef764d32f0971cc1276c252d7f0fd62
Eric Berquist <eric.berquist@gmail.com>
2022-01-30 14:45:30 -0500

    remove obsolete scripts

--- amber_prepare_pdb.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-keywords = ["ATOM", "HETATM", "TER", "END"]
-
-for word in keywords:
--- fixme_batch_divide_into_folders.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-def chunks(l, n):
-    """Yield successive n-sized chunks from l."""
-    for i in range(0, len(l), n):
-        yield l[i:i+n]
